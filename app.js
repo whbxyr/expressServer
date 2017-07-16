2e814aee2fbdd33ee1fdb903d3e598bfd43eb155
whbxyr <whbxyr@gmail.com>
2017-07-16 20:44:14 +0800

    成功代理跨域请求

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 var express = require('express');
 var request = require('request');
 var path = require('path');
-var host = 'http://api.haibian.com';
+var url = require('url');
+
 var app = express();
+var host = 'http://api.haibian.com';
 
 app.use(express.static(path.join(__dirname, './src')));
+
 app.get('/', function (req, res) {
 });
 
-app.get('/courseList/getLatest24HourChapterList', function (req, res) {
+app['get']('/courseList/getLatest24HourChapterList', function (req, res) {
   request({
-    url: host + '/courseList/getLatest24HourChapterList'
+    url: host + url.parse(req.url).pathname
   }, function (error, response, body) {
     res.send(body);
   });
 });
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+var testObj = {
+  'locals.title': app.locals.title,
+  'locals.email': app.locals.email
+};
+console.log(testObj);
