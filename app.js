b2a95fa45550f72cd757981751339ad872b197a6
whbxyr <whbxyr@gmail.com>
2017-07-20 12:58:43 +0800

    利用 ejs 模板初步完成界面及功能

--- app.js
+++ app.js
@@ -8,31 +8,57 @@ var router = express.Router();
 var app = express();
 var host = 'http://api.haibian.com';
 
-app.use(express.static(path.join(__dirname, './static')));
+app.use(express.static(path.join(__dirname, './assets')));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.engine('html', require('ejs').renderFile);
 app.set('views', './views');
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
-// app.all('*', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
-function reqPromise(url, ) {
-  return new Promise()
-}
+// function reqPromise(url, type, params) {
+//   return new Promise(function (resolve, reject) {
+//     request.
+//   });
+// }
 
 app.get('/', function (req, res) {
   request({
-    url: host + url.parse(req.url).pathname
+    // url: host + url.parse(req.url).pathname
+    url: host + '/courseList/getLatest24HourChapterList',
   }, function (error, response, body) {
-    res.send(body);
+    body = JSON.parse(body);
+    var data = [];
+    if (body && body.message === 'success') {
+      data = body.data;
+    }
+    res.render('index', {
+      data: data
+    });
   });
+  
+  // request
+  //   .get(host + '/courseList/getLatest24HourChapterList')
+  //   .on('response', function (response) {
+  //     response.on('data', function (data) {
+  //       res.render('index', {
+  //         message: JSON.stringify(data)
+  //       });
+  //     });
+  //   });
+    // .on('data', function (data) {
+    //   res.render('index', {
+    //     message: JSON.stringify(data)
+    //   });
+    // });
 });
 
 app.get('/courseList/getLatest24HourChapterList', function (req, res) {
